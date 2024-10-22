d26221253a771a14054c2af4bc4bd27017dac0a3
Tenro129 <nxgk4l@gmail.com>
2024-10-22 12:14:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 pkg install x11-repo -y ; pkg install termux-x11-nightly -y ; pkg install fluxbox -y ; pkg install thunar -y
 
-bash flux.sh
-
 echo -e "\033[1;31m [!] Installation is completed, run termux-x11 application to start.\033[0m"
 echo "Note: You can run ./flux.sh next time."
+
+./flux.sh
