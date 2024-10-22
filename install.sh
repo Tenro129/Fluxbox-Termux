fd0c9bbfd5da92278b90b2952180a3c964fc8c8e
Tenro129 <nxgk4l@gmail.com>
2024-10-22 12:15:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,5 @@ pkg install x11-repo -y ; pkg install termux-x11-nightly -y ; pkg install fluxbo
 echo -e "\033[1;31m [!] Installation is completed, run termux-x11 application to start.\033[0m"
 echo "Note: You can run ./flux.sh next time."
 
+chmod+x flux.sh
 ./flux.sh
