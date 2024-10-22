e77e9e7981caf154318dcf489f393fb2b5338475
Tenro129 <nxgk4l@gmail.com>
2024-10-22 12:26:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@ clear
 
 pkg install x11-repo -y ; pkg install termux-x11-nightly -y ; pkg install fluxbox -y ; pkg install thunar -y
 
-echo -e "\033[1;34m [!] Installation is completed, run termux-x11 application to start.\033[0m"
-echo "Note: You can run ./flux.sh next time."
-
+echo -e "\033[1;32m [!] Installation is completed, run termux-x11 application to start.\033[0m"
+echo "Note: You can run \033[0;32m./flux.sh\033[0m next time."
+echo "To stop the session, use \033[0;32mCtrl+C\033[0m"
+echo "This is not 'full desktop' environment, Termux doesn't support hardware acceleration."
+echo "Click anywhere on the screen with two fingers to see the available menu"
 chmod +x flux.sh
 ./flux.sh
